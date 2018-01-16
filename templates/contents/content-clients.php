811111213b6affa0b41efd288645fd78cd437154
Austin Winnett <austinjwinnett@gmail.com>
2018-01-16 16:52:58 -0600

    Added max width to imgs, pulling smaller images on client grid thing

--- templates/contents/content-clients.php
+++ templates/contents/content-clients.php
@@ -40,7 +40,7 @@ $clients = get_posts($args);
     <div class="clients__grid">
       <?php foreach( $clients as $key => $client ) : ?>
         <?php if ( get_the_post_thumbnail_url($client->ID) ) : ?>
-          <div class="clients__client" style="background-image: url(<?php echo get_the_post_thumbnail_url($client->ID); ?>);">
+          <div class="clients__client" style="background-image: url(<?php echo wp_get_attachment_image_url(get_post_thumbnail_id($client->ID), 'medium_large'); ?>);">
             <a href="<?php echo get_permalink($client->ID); ?>" class="overlay-link"></a>
             <span class="title-heading"><?php echo $client->post_title; ?></span>
           </div>
